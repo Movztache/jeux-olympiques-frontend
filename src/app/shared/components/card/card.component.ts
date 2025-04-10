9d382556b00027bffd069a919635de7715aedebe
Movztache <Movmikame@gmail.com>
2025-04-10 17:53:37 +0200

    add component card and modif home

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {NgClass, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  standalone: true,
+  imports: [NgIf, NgClass],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
+  @Input() cardType: 'sport' | 'news' = 'sport';
+  @Input() imgSrc: string = '';
+  @Input() imgAlt: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() linkUrl: string = '';
+  @Input() linkText: string = 'Lire la suite';
 }
