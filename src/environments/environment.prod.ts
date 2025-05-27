dea53885d79a020b64e5804127eb0cae36ed9c2d
Movztache <Movmikame@gmail.com>
2025-05-27 11:45:52 +0200

    change name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://votre-api-production.com/api'
+  apiUrl: 'https://api.vibe-ticket.com/api'
 };
